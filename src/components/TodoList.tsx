1bebf08464cacbdd6824019a409c17916a7585c3
“Leslie” <lesliecastellon06@icloud.com>
2026-04-05 22:32:14 -0700

    part 1.3 i think

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,41 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import type { Task } from "../types/taskTypes";
 
 function TodoList() {
   const [newTaskName, setNewTaskName] = useState<string>("");
-  const [newTaskDescription, setNewTaskDescription] = useState<string>("");
+  const [newTaskDeadline, setNewTaskDeadline] = useState<string>("");
+  const [todoList, setTodoList] = useState<Task[]>([]);
+
+  function addTask(taskName: string, deadline: string) {
+    if (taskName.trim() === "") {
+      return;
+    }
+
+    const newTask: Task = {
+      id: uuidv4(),
+      name: taskName,
+      deadline: deadline,
+    };
+
+    setTodoList([...todoList, newTask]);
+
+    // clear inputs
+    setNewTaskName("");
+    setNewTaskDeadline("");
+  }
 
   return (
     <div>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTask(newTaskName, newTaskDeadline);
+        }}
+      >
         <input
           type="text"
           placeholder="Enter a task"
-          name="task"
           value={newTaskName}
           onChange={(e) => setNewTaskName(e.target.value)}
           required
@@ -18,18 +43,15 @@ function TodoList() {
 
         <input
           type="text"
-          placeholder="Enter a description"
-          name="description"
-          value={newTaskDescription}
-          onChange={(e) => setNewTaskDescription(e.target.value)}
-          required
+          placeholder="Enter a deadline"
+          value={newTaskDeadline}
+          onChange={(e) => setNewTaskDeadline(e.target.value)}
         />
 
         <button type="submit">Add Task</button>
       </form>
 
-      <p>Task Name: {newTaskName}</p>
-      <p>Task Description: {newTaskDescription}</p>
+      <p>{JSON.stringify(todoList)}</p>
     </div>
   );
 }
--- src/types/taskTypes.ts
+++ src/types/taskTypes.ts
@@ -1,7 +1,5 @@
 export type Task = {
   id: string;
-  taskName: string;
-  deadline?: number;
-  completed: boolean;
+  name: string;
+  deadline: string;
 };
-
